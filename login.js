5e49a55ff4c55773d0213c3400359c55e316a7ef
bloodking1904 <macielsevero19@gmail.com>
2025-01-10 16:37:17 -0400

    Update login.js

--- login.js
+++ login.js
@@ -26,7 +26,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     // Verifica as credenciais do usuário
     if (users[username] && users[username] === password) {
         localStorage.setItem('loggedInUser', username);
-        window.location.href = 'veiculos.html';
+        window.location.href = 'agenda_veiculos.html';
     } else {
         alert('Usuário ou senha incorretos.');
     }
